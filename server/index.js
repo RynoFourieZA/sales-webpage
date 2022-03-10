86888f23cb85460444259986dd558559dbcf0fca
RynoFourieZA <rjrfourie@outlook.com>
2022-03-10 22:46:26 +0200

    Heroku Deployment checklist

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 3000;
  
 
 app.get("/", (req, res) => {
     res.send({ hi: "there" })
 })
 
-app.listen("3000", function () {
+app.listen(PORT, function () {
     console.log("Server is listening on port 3000. Ready to accept requests!");
 })
